c37bb015dc491202d2c393dde0e2de163b4285ac
maieul <maieul@maieul.net>
2013-05-02 18:27:45 +0200

    unification des types d'indentation + listage des variantes

--- froger.py
+++ froger.py
@@ -13,14 +13,46 @@ def lecture_ligne(ligne):
 	#Supprimer les commentaires
 	comment = ligne.find(config.comment)
 	ligne = ligne[:comment]
-	return ligne
-	    
+	
+	if len(ligne) == 0:
+		return (False,False)
+	#pour la liste des sigles
+	if ligne[0:len(config.sigle)] == config.sigle:
+	   sigles = ligne[len(config.sigle):]
+	   return config.sigle,sigles.split(config.sep_temoin)
+	
+	#pour dectecter qu'on passe aux variantes
+	elif ligne.find(config.sep_variante):
+	    variante = ligne.split(config.sep_variante)
+	if len(variante) != 2:
+		print ("Lieu n'ayant pas exactement deux variantes : "+ligne)
+	else:
+		variante[0] = variante[0].split(config.sep_temoin)
+		variante[1] = variante[1].split(config.sep_temoin)
+		return variante
+	
+	return (False,False)
+
+
 	
 def lecture_fichier(fichier):
-        """Lire le fichier, ligne par ligne"""
-        fichier = open(fichier,"r")
-        for ligne in fichier:
-            print(lecture_ligne(ligne))
+	"""Lire le fichier, ligne par ligne"""
+	fichier = open(fichier,"r")
+	variantes = []
+	for ligne in fichier:
+	    analyse =  lecture_ligne(ligne)
+	    
+	    # trouver les sigles
+	    if analyse[0] == (config.sigle):
+		    sigle = analyse[1]
+		    
+	    #trouver les variantes
+	    elif analyse != (False,False):
+		    variantes.append(analyse)
+	if !sigle:
+		print "Sigles non définis : erreur"
+		break
+
 
 def __main__():
 	import sys
